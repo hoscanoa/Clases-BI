5fe64f139c7867c6e6ab3354ce8c12383b321eb6
hoscanoa <hoscanoa@gmail.com>
2015-09-07 15:27:06 -0500

    Semana 1 y 2
    Semana 1 y 2

---
+++ Semana1/SQLQuery2.sql
@@ -0,0 +1,15 @@
+/*Data Warehouse*/
+USE AdventureWorksDW2014
+GO
+
+SELECT * FROM sys.tables
+GO
+
+SELECT *  FROM FactInternetSales
+GO
+
+SELECT * FROM DimProduct
+GO
+
+SELECT *  FROM FactFinance
+GO
---
+++ Semana1/SQLQuery3.sql
@@ -0,0 +1,6 @@
+/*Database*/
+USE AdventureWorks2014
+GO
+
+SELECT * FROM sys.tables
+GO
